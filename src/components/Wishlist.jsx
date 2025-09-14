7e7ce71be116f7735d70cf277d6150d76dcb0f33
Eng-Mohamed-Raya <mohamedyasserraya@gmail.com>
2025-09-14 14:07:52 +0300

    Fix/ wishlist loading and empty logics

--- src/components/Wishlist.jsx
+++ src/components/Wishlist.jsx
@@ -23,14 +23,13 @@ function Wishlist() {
     }
       };
     return (<div className="container" style={{minHeight:"90vh"}}>
-      {loading && <Loading/>}
        <div className="d-flex align-items-center justify-content-between my-5">
         <h1>Wishlist ({wishlistData.length})</h1> 
         <Button name="Add all to Cart"/>
        </div>
          <div className="products-container ">
-                        {wishlistData && wishlistData?.map((product)=><Card key={product._id} {...product}><i className="fa-solid fa-trash"  onClick={()=>handelDeleteFromWishlist(product._id)}></i></Card>)} 
-                        {(loading && wishlistData )|| <h1 className="center" style={{color:"var(--secondary-color)"}}> Your wishlist is empty.</h1>}   
+                      {loading ? <Loading/> : wishlistData ? wishlistData?.map((product)=><Card key={product._id} {...product}><i className="fa-solid fa-trash"  onClick={()=>handelDeleteFromWishlist(product._id)}></i></Card>)
+                       : <h1 className="center" style={{color:"var(--secondary-color)"}}> Your wishlist is empty.</h1>}   
           </div>
     </div>);
 }
