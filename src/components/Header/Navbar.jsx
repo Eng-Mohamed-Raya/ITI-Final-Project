351530df3e1a0704c224330188eea7c0a0382a9e
Eng-Mohamed-Raya <mohamedyasserraya@gmail.com>
2025-09-14 14:33:11 +0300

    Fix/navbar

--- src/components/Header/Navbar.jsx
+++ src/components/Header/Navbar.jsx
@@ -18,19 +18,19 @@ function Navbar() {
                 <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse m-auto" id="navbarSupportedContent" >
-                    <ul className="navbar-nav ms-auto mb-2 mb-lg-0 gap-lg-4 ">
+                    <ul className="navbar-nav ms-auto mb-2 mb-lg-0 gap-xl-4 ">
                         <li className="nav-item">
                             <NavLink className="nav-link " aria-current="page" to="/">Home</NavLink>
                         </li>
                         <li className="nav-item">
                             <NavLink className="nav-link" to="/about">About</NavLink>
                         </li>
+                        {userInfo?.role=="ADMIN" ? <li className="nav-item"><NavLink className="nav-link" to="/admin">Dashboard</NavLink></li>:
                         <li className="nav-item">
                             <NavLink className="nav-link " aria-current="page" to="/contact">Contact</NavLink>
-                        </li>
+                        </li>}
                         <li className="nav-item">
-                             {userInfo?.role=="ADMIN" && <NavLink className="nav-link" to="/admin">Dashboard</NavLink>
-                           }
+                            <NavLink className="nav-link" to="/products">Products</NavLink>
                         </li>
                         <li className="nav-item">
                              {userInfo?.isLoged ?
